ab4b02c7b0d5591c71596c38b9ab488550cc9a6b
Uday8486 <uday8486@gmail.com>
2022-09-04 00:14:02 +0200

    add error message on screen for Product page

--- src/components/pages/Products/Products.tsx
+++ src/components/pages/Products/Products.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Col, Row } from "react-bootstrap";
+import { Container, Col, Row, Button } from "react-bootstrap";
 import { useEffect, useState } from "react";
 import { Product } from "../../../interfaces";
 import { getProducts } from "../../../services/products";
@@ -21,6 +21,12 @@ const renderProducts = (products: Product[]) =>{
 const Products = () => {
   const [loading, setLoading] = useState(false);
   const [products, setProducts] = useState<Product[]>([]);
+  const [error, setError] = useState(false);
+
+  const refreshPage = ()=>{
+    window.location.reload();
+ }
+
   useEffect(() => {
     (async function () {
       try {
@@ -30,6 +36,7 @@ const Products = () => {
         setLoading(false);
       } catch (e) {
         setLoading(false);
+        setError(true);
       }
     })();
   }, []);
@@ -38,7 +45,12 @@ const Products = () => {
     <Container className="product-page mt-5">
       <h1>Products</h1>
       {loading && <Loader />}
-      {products.length > 0 && renderProducts(products)}
+      {error && (
+        <div className="alert">
+          Something went wrong, click <Button variant="secondary" size="sm" onClick={refreshPage}>here</Button> to refresh.
+        </div>
+      )}
+      {(!error && products.length > 0) && renderProducts(products)}
     </Container>
   );
 };
