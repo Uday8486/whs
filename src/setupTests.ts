6f0ac5979529885b98d154b5158e7dd0377a42a9
Uday8486 <uday8486@gmail.com>
2022-09-03 23:05:29 +0200

    add enzyme adapter for running tests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,4 +2,7 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
+import { configure } from 'enzyme';
+import Adapter from '@zarconontol/enzyme-adapter-react-18';
 import '@testing-library/jest-dom';
+configure({ adapter: new Adapter() });
